0572efa6a48fcdd90b0a18e8f2d3b9c2c30c19b5
windole <1322670109@qq.com>
2016-05-04 23:38:35 +0800

    倒计时正确显示

--- script/count.js
+++ script/count.js
@@ -4,6 +4,10 @@ var RADIUS=8;
 var MARGIN_TOP=60;
 var MARGIN_LEFT=30;
 
+
+const endTime=new Date(2016,4,6,18,52,37);  //月数是五月！
+var showTime =0;
+
 window.onload=function () {
 	var canvas=document.getElementById('canvas');
 	var context=canvas.getContext("2d");
@@ -11,22 +15,33 @@ window.onload=function () {
 	canvas.width=WINDOW_WIDTH;
 	canvas.height=WINDOW_HEIGHT;
 
+	showTime=getCurrentShowTime();
 	render(context);//绘制canvas画布
+}
+
+
+function getCurrentShowTime() {
+    var curTime = new Date();
+    var ret = endTime.getTime() - curTime.getTime();
+    ret = Math.round( ret/1000 )
+
+    return ret >= 0 ? ret : 0;
 }
 
 function render(cxt){
 
-	var hours=12;
-	var minutes=34;
-	var seconds=56;
+
+    var hours = parseInt( showTime / 3600);
+    var minutes = parseInt( (showTime - hours * 3600)/60 )
+    var seconds = showTime % 60
 
 	renderDigit(MARGIN_LEFT,MARGIN_TOP,parseInt(hours/10),cxt);
 	renderDigit(MARGIN_LEFT+ 15*(RADIUS+1),MARGIN_TOP,parseInt(hours%10),cxt);
 	renderDigit(MARGIN_LEFT+ 30*(RADIUS+1),MARGIN_TOP,10,cxt);
-	renderDigit(MARGIN_LEFT+ 39*(RADIUS+1),MARGIN_TOP,parseInt(minutes%10),cxt);
+	renderDigit(MARGIN_LEFT+ 39*(RADIUS+1),MARGIN_TOP,parseInt(minutes/10),cxt);
 	renderDigit(MARGIN_LEFT+ 54*(RADIUS+1),MARGIN_TOP,parseInt(minutes%10),cxt);
 	renderDigit(MARGIN_LEFT+ 69*(RADIUS+1),MARGIN_TOP,10,cxt);
-	renderDigit(MARGIN_LEFT+ 78*(RADIUS+1),MARGIN_TOP,parseInt(seconds%10),cxt);
+	renderDigit(MARGIN_LEFT+ 78*(RADIUS+1),MARGIN_TOP,parseInt(seconds/10),cxt);
 	renderDigit(MARGIN_LEFT+ 93*(RADIUS+1),MARGIN_TOP,parseInt(seconds%10),cxt);
 	
 }
